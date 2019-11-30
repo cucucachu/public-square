cd6e7e086dafd214bdaf042eae6950366241ac2d
Cody Jones <codymjones@ymail.com>
2019-11-29 18:51:57 -0800

    Attribute.js documented.

--- src/noomman/Attribute.js
+++ src/noomman/Attribute.js
@@ -1,11 +1,42 @@
+/*
+ * Class Attribute
+ * Describes the properties and rules/requirements for a single attribute (non-ObjectId property) 
+ *   of a ClassModel. 
+ */
+
 class Attribute {
 
+    /* 
+     * Attribute constructor
+     * Parameters
+     * - attributeSchema - An object describing an attribute. 
+     * {
+     *  name: String (required), 
+     *  type: String (required), 
+     *  list: Boolean,
+     *  required: Boolean,
+     *  unique: Boolean,
+     *  sensitive: Boolean,
+     *  mutex: String,
+     *  requiredGroup: String,
+     * }
+     */
     constructor(attributeSchema) {
         this.constructorValidations(attributeSchema);
 
         Object.assign(this, attributeSchema);
     }
 
+    /* 
+     * constructorValidations(attributeSchema)
+     * This method validates that the attributeSchema object passed to the Attribute constructor are valid values.
+     * Parameters
+     * - attributeSchema - Object - The same object that the constructor() method accepts.
+     * Returns
+     * - Nothing
+     * Throws
+     * - Error - When the attributeSchema properties are of incorrect type or conflict with one another.
+     */
     constructorValidations(attributeSchema) {
         if (!attributeSchema.name) {
             throw new Error('Attempt to create an attribute without a name.');
@@ -39,10 +70,25 @@ class Attribute {
         }
     }
 
+    /* 
+     * validTypes()
+     * This method returns the valid data types for an attribute.
+     * Returns
+     * - An array containing valid data types for an attribute.
+     */
     static validTypes() {
         return [Number, String, Boolean, Date];
     }
 
+    /* 
+     * isSet(value)
+     * Determines if a given value for this Attribute is considered as 'set'. 
+     * A set value is one which is not null, or not an empty array.
+     * Parameters
+     * - value - any type - a value from an Instance of the ClassModel which holds this Attribute.
+     * Returns
+     * - True is value is considered set for this Attribute, false otherwise.
+     */
     isSet(value) {
         if (this.list) {
             if (!Array.isArray(value) || value.length === 0)
@@ -56,6 +102,16 @@ class Attribute {
         return true;
     }
 
+    /* 
+     * validate(value)
+     * Validates a given value for the type and restrictions defined in this Attribtue.
+     * Parameters
+     * - value - any type - a value from an Instance of the ClassModel which holds this Attribute.
+     * Returns
+     * - undefined
+     * Throws
+     * - Error - when given value is not valid for this Attribute.
+     */
     validate(value) {
         if (value === null)
             return true;
@@ -81,6 +137,14 @@ class Attribute {
         }
     }
 
+    /* 
+     * validType(value)
+     * Validates a given value is of correct type for this attribute.
+     * Parameters
+     * - value - any type - a value from an Instance of the ClassModel which holds this Attribute.
+     * Returns 
+     * - True if value is of the correct type for this Attribute, False otherwise.
+     */
     validType(value) {
         if (value === null)
             return true;
