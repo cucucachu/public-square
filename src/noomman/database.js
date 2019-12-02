7b7dc0142e18081158474080152dcbf5a21f80bf
Cody Jones <codymjones@ymail.com>
2019-12-02 11:25:21 -0800

    Added comments to database.js

--- src/noomman/database.js
+++ src/noomman/database.js
@@ -1,5 +1,8 @@
-// MongoDB and Mongoose Setup
-// If you are a developer running this on a local machine, make sure you whitelist your IP address on mongodb atlas.
+/*
+ * database
+ * Holds all the code related to connecting to and interacting with a mongo database.
+ */
+
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const mongo_uri = "mongodb+srv://cody_jones:cody_jones@publicsquaredev-d3ue6.gcp.mongodb.net/test?retryWrites=true";
@@ -7,6 +10,17 @@ const mongo_uri = "mongodb+srv://cody_jones:cody_jones@publicsquaredev-d3ue6.gcp
 let client = null;
 let db = null;
 
+/*
+ * connect(uri)
+ * Connects to the mongo database with the given uri.
+ * Parameters
+ * uri - String - A uri string of the database to connect to.
+ * Returns
+ * - Promise<mongodb.Db> - The database object returned by mongodb.MongoClient.connect().
+ * Throws
+ * - Error - If an attempt is made to connect to a database more than once without calling close().
+ * - MongoError - If underlying call to mongodb.MongoCLient.connect() throws an error.
+ */
 async function connect() {
 	if (connected()) {
 		throw new Error('Attempt to connect to database twice.');
@@ -19,16 +33,37 @@ async function connect() {
 	return db;
 }
 
+/* 
+ * close()
+ * Closes the connection to a mongo db that was established by calling connect().
+ */
 async function close() {
 	await client.close();
 	db = null;
 	client = null;
 }
 
+/* 
+ * connected()
+ * Use to determine if a connection to a database is active.
+ * Returns
+ * - Boolean - True if connection to database is active, false otherwise.
+ */
 function connected() {
 	return db !== null && client !== null;
 }
 
+/*
+ * index(collection, field)
+ * Adds an index on the given field to the collection with the given name in the database.
+ *    Calls native mongodb function mongodb.Collection.createIndex()
+ * Parameters
+ * - collection - String - the name of the collection to add an index to.
+ * - field - String | Array | Object - see fieldOrSpec parameter 
+ *      https://mongodb.github.io/node-mongodb-native/3.3/api/Collection.html#createIndex
+ * Returns 
+ * - Promise<result> - String representing all the indices added to the collection.
+ */
 async function index(collection, field) {
 	if (!connected()) {
 		throw new Error('database.index() called before database.connect()')
@@ -36,6 +71,16 @@ async function index(collection, field) {
 	return db.collection(collection).createIndex(field);
 }
 
+/*
+ * ObjectId(hexString)
+ * Creates a new ObjectId either from the given hexString or by calling new mongodb.ObjectId().
+ * Parameters
+ * - hexString - String - A 24 byte hex string.
+ * Returns
+ * - mongodb.ObjectId - a native mongo ObjectId object.
+ * Throws
+ * - MongoError - if given hexString is invalid.
+ */
 function ObjectId(hexString) {
 	if (hexString) {
 		return ObjectIdFromHexString(hexString);
@@ -44,29 +89,75 @@ function ObjectId(hexString) {
 	return new mongo.ObjectId();
 }
 
+/*
+ * ObjectIdFromHexString(id)
+ * Creates a new ObjectId from the given hexString by calling mongodb.ObjectId.createFromHexString().
+ * Parameters
+ * - id - String - A 24 byte hex string.
+ * Returns
+ * - mongodb.ObjectId - a native mongo ObjectId object.
+ * Throws
+ * - MongoError - if given hexString is invalid.
+ */
 function ObjectIdFromHexString(id) {
 	return mongo.ObjectId.createFromHexString(id);
 }
 
+/*
+ * insertOne(collection, document)
+ * Inserts a single document into the given collection.
+ * Parameters
+ * - collection - String - the name of the collection in which to insert the document.
+ * - document - Object - a document to insert into the given collection.
+ * Returns
+ * - Promise<insertOneWriteOpResult> - see https://mongodb.github.io/node-mongodb-native/3.3/api/Collection.html#~insertOneWriteOpResult
+ */
 async function insertOne(collection, document) {
 	return db.collection(collection).insertOne(document);
 }
 
-async function insertMany(collection, docs) {
-	return db.collection(collection).insertMany(docs);
+/*
+ * insertMany(collection, documents)
+ * Inserts given documents into the given collection.
+ * Parameters
+ * - collection - String - the name of the collection in which to insert the documents.
+ * - documents - Array<Object> - a array of documents to insert into the given collection.
+ * Returns
+ * - Promise<insertWriteOpResultObject> - see https://mongodb.github.io/node-mongodb-native/3.3/api/Collection.html#~insertWriteOpResult
+ */
+async function insertMany(collection, documents) {
+	return db.collection(collection).insertMany(documents);
 }
 
-async function overwrite(collection, doc) {
+/* 
+ * overwrite(collection, instance)
+ * Overwrites the given instance in the given collection. Do Not Use. Internal Use Only.
+ * Parameters
+ * - collection - String - the name of the collection in which to overwrite the instance.
+ * - instance - Instance - An instance to overwrite in the database.
+ * Returns
+ * - Promise<updateWriteOpResult> - see https://mongodb.github.io/node-mongodb-native/3.3/api/Collection.html#~updateWriteOpResult
+ */
+async function overwrite(collection, document) {
 	const filter = {
-		_id: doc._id,
+		_id: document._id,
 	};
 	const update = {
-		$set: doc
+		$set: document
 	}
 
 	return db.collection(collection).updateOne(filter, update);
 }
 
+/* 
+ * update(collection, instance)
+ * Updates the given instance in the given collection.
+ * Parameters
+ * - collection - String - the name of the collection in which to update the instance.
+ * - instance - Instance - An instance to update in the database.
+ * Returns
+ * - Promise<updateWriteOpResult> - see https://mongodb.github.io/node-mongodb-native/3.3/api/Collection.html#~updateWriteOpResult
+ */
 async function update(collection, instance) {
 	const filter = {
 		_id: instance._id,
@@ -76,14 +167,47 @@ async function update(collection, instance) {
 	return db.collection(collection).updateOne(filter, update);
 }
 
+/*
+ * find(collection, query)
+ * Runs the given query on the given collection using the native mongodb.Collection.find() function.
+ *    Returns the documents which match the query as an array. Do not use with queries expected to return
+ *    a large number of documents.
+ * Parameters
+ * - collection - String - the name of the collection in which to search for documents.
+ * - query - Object - A mongo query object.
+ * Returns
+ * Promise<Array<Object>> - An array of all the objects in the given collection which match the given query.
+ */
 async function find(collection, query) {
 	return db.collection(collection).find(query).toArray();
 }
 
+/*
+ * findOne(collection, query)
+ * Runs the given query on the given collection using the native mongodb.Collection.findOne() function.
+ *    Returns the first document which matches the query. Returns null if no document matches the query.
+ * Parameters
+ * - collection - String - the name of the collection in which to search for a document.
+ * - query - Object - A mongo query object.
+ * Returns
+ * Promise<Object> - The first document in the collection which matches the query. Null if no document 
+ *    matches the query.
+ */
 async function findOne(collection, query) {
 	return db.collection(collection).findOne(query);
 }
 
+/*
+ * findById(collection, id)
+ * Queries the given collection for a document with the given Object Id. Returns null if no document 
+ *    has the given ObjectId.
+ * Parameters
+ * - collection - String - the name of the collection in which to search for a document.
+ * - id - mongodb.ObjectId - A mongo ObjectId to search the collection for.
+ * Returns
+ * Promise<Object> - The document in the collection with the given ObjectId. Null if no document 
+ *    in the collection has the given ObjectId.
+ */
 async function findById(collection, id) {
 	if (!(id instanceof mongo.ObjectID))
 		return null;
@@ -95,6 +219,15 @@ async function findById(collection, id) {
 	return findOne(collection, query);
 }
 
+/*
+ * deleteOne(collection, document)
+ * Deletes the document with the ObjectId of the given document from the given collection.
+ * Parameters
+ * - collection - String - the name of the collection containing the document to delete.
+ * - document - Object - a document to delete.
+ * Returns
+ * - Promise<deleteWriteOpResult> - see https://mongodb.github.io/node-mongodb-native/3.3/api/Collection.html#~deleteWriteOpResult
+ */
 async function deleteOne(collection, document) {
 	const filter = {
 		_id: document._id,
@@ -103,6 +236,15 @@ async function deleteOne(collection, document) {
 	return db.collection(collection).deleteOne(filter);
 }
 
+/*
+ * deleteMany(collection, documents)
+ * Deletes all of the given documents from the given collection.
+ * Parameters
+ * - collection - String - the name of the collection containing the documents to delete.
+ * - documents - Iterable<Object> - an Iterable (Array, InstanceSet, etc.) containing documents to delete.
+ * Returns 
+ * - Promise<deleteWriteOpResult> - see https://mongodb.github.io/node-mongodb-native/3.3/api/Collection.html#~deleteWriteOpResult
+ */
 async function deleteMany(collection, documents) {
 	const filter = {
 		_id: {
@@ -113,10 +255,26 @@ async function deleteMany(collection, documents) {
 	return db.collection(collection).deleteMany(filter);
 }
 
+/* 
+ * clearCollection(collection)
+ * Removes all documents from the given collection.
+ * Parameters
+ * - collection - String - the name of the collection to delete all documents from.
+ * Returns 
+ * - Promise<deleteWriteOpResult> - see https://mongodb.github.io/node-mongodb-native/3.3/api/Collection.html#~deleteWriteOpResult
+ */
 async function clearCollection(collection) {
 	return db.collection(collection).deleteMany({});
 }
 
+/* 
+ * collection(name)
+ * Retrieves the mongodb.Collection object for a collection with the given name in the connected database.
+ * Parameters
+ * - name - String - the name of the collection to retrieve.
+ * Returns
+ * - mongodb.Collection - The collection in the connected database matching the given name.
+ */
 function collection(name) {
 	if (db)
 		return db.collection(name);
