7312115f17563303e2f7dcd23a659e0b34ad0a5a
Cody Jones <codymjones@ymail.com>
2019-12-02 15:50:52 -0800

    Added comments to InstanceReference.js

--- src/noomman/InstanceReference.js
+++ src/noomman/InstanceReference.js
@@ -1,5 +1,16 @@
+/*
+ * class InstanceReference
+ * Used by InstanceState to hold the value of a singular relationship. This class helps the processing
+ *    and storing of singular relationships, since relationships will have an ObjectId as a value when 
+ *    retrieved from the database, and an Instance as a value when the relationship is set by a user 
+ *    or populated by calling Instance.walk().
+ */
 class InstanceReference {
 
+    /*
+     * constructor()
+     * Creates a new instance of InstanceReference.
+     */
     constructor() {
         this._id = null;
         this.instance = null;
@@ -14,14 +25,38 @@ class InstanceReference {
         });
     }
 
+    /*
+     * equals(that)
+     * Used to compare two InstanceReferences. InstanceReferences are equal if they have
+     *    the same id property.
+     * Parameters
+     * - that - InstanceReference - Another InstanceReference to compare against.
+     * Returns
+     * - Boolean - True if this InstanceReference's id is the same as the given InstanceReference's id.
+     */
     equals(that) {
         return this.id === that.id;
     }
 
+    /*
+     * isEmpty()
+     * Determines whether this is InstanceReference is set or empty.
+     * Returns
+     * - Boolean - True if this InstanceReference has the _id property set to a non-null value.
+     */
     isEmpty() {
         return this._id === null;
     }
 
+    /*
+     * diff(that)
+     * Creates a diff object based on the value of this InstanceReference as compared to the
+     *    given InstanceReference.
+     * Parameters
+     * - that - InstanceReference - Another InstanceReference to compare against.
+     * Returns
+     * - Object - a diff object in the format of a mongo update operation object.
+     */
     diff(that) {
         if (that === null) {
             return {
