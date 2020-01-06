688166a8084c7eb15eac3edaac93026c277561d5
Cody Jones <codymjones@ymail.com>
2020-01-06 10:36:32 -0800

    Removed all noomman classes and tests from repo

--- test/noomman/helpers/DatabaseConnection.js
+++
@@ -1,7 +0,0 @@
-const mongo_uri = "mongodb+srv://cody_jones:cody_jones@publicsquaredev-d3ue6.gcp.mongodb.net/test?retryWrites=true";
-const testDatabase = 'test';
-
-module.exports = {
-    mongo_uri,
-    testDatabase,
-}
