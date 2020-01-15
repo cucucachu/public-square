3b50ae000d1dc160a89a01f7e61e51b059115900
Cody Jones <codymjones@ymail.com>
2020-01-15 12:18:15 -0800

    Added routes for each of the MiraController functions. Tested each route using postman.

--- src/routes/mira.js
+++ src/routes/mira.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const miraController = require('../controllers/miraController');
+const MiraController = require('../controllers/MiraController');
 
 router.get('/schema', (request, response) => {
     try {
-        response.json(miraController.getClassModels());
+        response.json(MiraController.getClassModels());
     }
     catch (error) {
         response.status(500).json({ error: error.message });
     }
 });
 
-router.get('/schema/:className', (request, response, next) => {
+router.get('/schema/:className', (request, response) => {
     let schema;
     try {
-           schema = miraController.schemaForClassModel(request.params.className);
+           schema = MiraController.schemaForClassModel(request.params.className);
            response.json(schema);
     }
     catch (error) {
@@ -23,11 +23,39 @@ router.get('/schema/:className', (request, response, next) => {
     }
 });
 
-router.post('/', async (request, response) => {
+router.post('/get', async (request, response) => {
+    const instance = await MiraController.get(request.body);
+    response.json(instance);
+});
+
+router.post('/getInstances', async (request, response) => {
+    const data = request.body;
+    const instances = await MiraController.getInstances(
+        data.className, data.filter, data.page, data.pageSize, data.orderBy
+    );
+    response.json(instances);
+});
+
+router.post('/put', async (request, response) => {
     try {
-        await miraController.put(request.body);
+        const result = await MiraController.put(request.body);
         response.json({
             status: 'successful',
+            result,
+        });
+    }
+    catch (error) {
+        response.status(500).json({ error: error.message });
+    }
+});
+
+router.post('/delete', async (request, response) => {
+    try {
+        const result = await MiraController.deleteInstance(request.body);
+        response.json({
+            className: request.body.className,
+            id: request.body.id,
+            deleted: result,
         });
     }
     catch (error) {
